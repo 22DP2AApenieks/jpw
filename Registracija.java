ad01c7cb029cc0b9121d761b593d2d6176a1b236
22DP2AApenieks <131154370+22DP2AApenieks@users.noreply.github.com>
2023-12-19 13:13:19 +0200

    Update Registracija.java

--- Registracija.java
+++ Registracija.java
@@ -1,53 +1,72 @@
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.FileWriter;
 import java.io.IOException;
-
+//šis fails atvērsies ja lietotāja izvēle būs 1-reģistrēties
 public class Registracija {
-    private static ArrayList<String> Dalibnieki = new ArrayList<>();
-        Scanner scanner = new Scanner(system.in);
-        
-    
-        System.out.print("Ievadiet vārdu: ");
-        String name = scanner.nextLine();
-        
-        System.out.print("Ievadiet uzvārdu: ");
-        String surname = scanner.nextLine();
-        
-        System.out.print("Ievadiet pievilkšanās reižu rekordu: ");
-        String pullups = scanner.nextLine();
-        
-        System.out.print("Ievadiet piepumpēšanās reižu rekordu: ");
-        String pushups = scanner.nextLine();
-        
+    private static ArrayList<String> Dalibniekussaraksts = new ArrayList<>();
 
-        ArrayList<String> Dalibnieki = name + "," + surname + "," + pullups + "," + pushups;
-        studentList.add(Dalibnieki);
-        
-
-    private static void saveToCSV(ArrayList<String> Dalibnieki) {
+    public static void registracijaMetode(){
         try {
-            FileWriter csvWriter = new FileWriter(Dalibnieki.csv, true); 
+            Scanner scanner = new Scanner(System.in);
+        //uztaisu skeneri
+            System.out.print("Ievadiet vārdu: ");
+            String name = scanner.nextLine();
 
-            for (String data : Dalibnieki) {
-                csvWriter.append(Dalibnieki);
-                csvWriter.append(", ");
+            System.out.print("Ievadiet uzvārdu: ");
+            String surname = scanner.nextLine();
 
-                system.out.printline("");
-                 
+            System.out.print("Ievadiet pievilkšanās reižu rekordu: ");
+            String pullups = scanner.nextLine();
+
+            System.out.print("Ievadiet piepumpēšanās reižu rekordu: ");
+            String pushups = scanner.nextLine();
+            //saglabāju dažādu informāciju par lietotāju
+
+
+            String Dalibnieki = name + "," + surname + "," + pullups + "," + pushups;
+            Dalibniekussaraksts.add(Dalibnieki);
+            //apkiopo ievadītos datus
+
+            //sakas datu saglabāšana csv faila
+            saveToCSV("Dalibnieki.csv", Dalibniekussaraksts);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveToCSV(String fileName, ArrayList<String> dataList) {
+        try {
+            FileWriter csvWriter = new FileWriter(fileName, true); 
+
+            for (String data : Dalibniekussaraksts) {
+                csvWriter.append(data);
+                csvWriter.append("\n");
             }
 
             csvWriter.flush();
             csvWriter.close();
 
-            System.out.println("Student information has been saved to Dalibnieki.csv");
+            System.out.println("Dalībnieka informācija saglabāta " + fileName);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    
 
+    public static void registracija() {
+        
+    }
+}
 
-    public Dalibnieki(String name, String surname, int pullups, int pushups) {
+class Dalibnieks {
+    public String name;
+    public String surname;
+    public int pullups;
+    public int pushups;
+
+    public Dalibnieks(String name, String surname, int pullups, int pushups) {
         this.name = name;
         this.surname = surname;
         this.pullups = pullups;
