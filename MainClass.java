b258c990ee40d51736fa86a5cdb35e3ce4e2ba1e
22DP2AApenieks <131154370+22DP2AApenieks@users.noreply.github.com>
2023-12-19 13:12:11 +0200

    Update and rename Galvenaprogrmma.java to MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,27 @@
+//galvenā programma ar kuru programāa sāksies
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+
+        //printē informāciju lietotājam, lai tas spētu lietot programmu
+        System.out.println("Izvēlieties darbību:");
+        System.out.println("1. Reģistrēt lietotāju");
+        System.out.println("2. Ieiet litotāja profilā");
+        System.out.println("3. Iziet");
+        Scanner scanner = new Scanner(System.in);
+        //izveidojam skeneri
+
+
+        int izvele = Integer.valueOf(scanner.nextLine());
+        if (izvele == 1){
+            Registracija.registracijaMetode();
+        } else if (izvele == 2){
+            Ieiesana.ieiesanasMetode();
+        } else {
+            System.exit(0);
+        }
+        //izvēle salīdzina lietotāja ievadi un pēc tās novirza lietotāju uz reģistrāciju vai ieiešanu, vai beidz programmu
+    }
+}
